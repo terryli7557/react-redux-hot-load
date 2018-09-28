48acfb1f4329c7bca410f1d2a6158847dfcdefdb
terry <terryli7557@gmail.com>
2018-09-28 10:00:06 +1000

    update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,6 @@ module.exports = {
   "parser": "babel-eslint",
   "rules": {
     "react/prop-types": 0,
-    "class-methods-use-this": 0,
-    "import/no-named-as-default": 0,
-    "react/jsx-filename-extension": [
-      "error",
-      {
-        "extensions": [".js", ".jsx"]
-      }
-    ]
+    "class-methods-use-this": 0
   }
 };
